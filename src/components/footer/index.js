06fa1c9616052dd8bf9786d159dfec529c85e9ae
Scott Lease <79185881+scottie-l@users.noreply.github.com>
2022-03-15 10:11:52 -0700

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './footer.scss';
 
-class Footer extends React.Component {
-    render() {
-      return <footer>&copy; 2018;</footer>;
-  };
-};
+function Footer() {
+  return <footer>&copy; 2022;</footer>;
+}
 
 export default Footer;
