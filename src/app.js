345225880ac34bb48a48da3b6fdc79afca580954
Scott Lease <79185881+scottie-l@users.noreply.github.com>
2022-03-15 10:10:05 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,37 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import './App.scss';
 
-import './app.scss';
-
-// There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
 
-class App extends React.Component {
+function App() {
+  let [data, setData] = useState(null);
+  let [requestParams, setRequest] = useState({});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  };
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
+  const callApi = (requestParams) => {
+    data = {
       count: 2,
       results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
       ],
     };
-    this.setState({data, requestParams});
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall = {this.callApi} />
-        <Results data = {this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
+    setData({ data, requestParams });
   };
-};
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: { requestParams.method }</div>
+      <div>URL: { requestParams.url }</div>
+      <Form handleApiCall={ callApi } setRequest={ setRequest } requestParams={requestParams}/>
+      <Results data={ data } />
+      <Footer />
+    </React.Fragment>
+  );
+}
 
 export default App;
