87e0fbb702a00b1fea6a61db01821c49de72a921
Scott Lease <79185881+scottie-l@users.noreply.github.com>
2022-03-15 10:14:06 -0700

    Update index.js

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './results.scss';
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  };
-};
+function Results() {
+  return (
+    <section>
+      <pre>{ this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null }</pre>
+    </section>
+  );
+}
 
 export default Results;
